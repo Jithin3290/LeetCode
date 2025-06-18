f8c02a7ccecfb09d68490ba1bbe8e1cd404eaace
Jithin3290 <jithin3290@gmail.com>
2025-06-18 15:20:16 +0530

    Time: 11 ms (14.15%) | Memory: 60.7 MB (69.12%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let tot = 0;
+      const freq = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    for (let i = 0;i < s.length; i++)
+    {
+        let c = freq[s[i]]
+        let n = freq[s[i+1]]
+        if(c < n)
+        {
+            tot+= (n-c)
+            i = i+1
+        }
+        else
+        {
+            tot+= c
+        }
+    }
+    return tot
+};
