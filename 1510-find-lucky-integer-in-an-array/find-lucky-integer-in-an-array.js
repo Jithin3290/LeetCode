fead0af56a706301c50ec2199370da5bf2ad41c6
Jithin C <jithin3290@gmail.com>
2025-07-08 15:13:30 +0530

    Time: 1 ms (91.96%) | Memory: 56.2 MB (29.68%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function(arr) {
+    let freq = {}
+    for(let i of arr){
+        freq[i] = (freq[i]||0) + 1
+    }
+    let max = [-1]
+    for (let i in freq){
+        if(parseInt(i) === freq[i]){
+            max.push(parseInt(i))
+        }
+        
+    }
+    return Math.max(...max)
+};
