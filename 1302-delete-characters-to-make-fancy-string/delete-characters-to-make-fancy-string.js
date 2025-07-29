97de26e6af9381b73e33caf06cf9d45bf1ef258d
Jithin C <jithin3290@gmail.com>
2025-07-29 16:05:36 +0530

    Time: 65 ms (49.41%) | Memory: 81.8 MB (10.83%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+  const res = [];
+    for (let i = 0; i < s.length; i++) {
+        const len = res.length;
+        if (len >= 2 && res[len - 1] === s[i] && res[len - 2] === s[i]) {
+            continue; 
+        }
+        res.push(s[i]);
+    }
+    return res.join('');
+}
