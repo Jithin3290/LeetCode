da1b6dfe398d52918e3694ff02f638d79fa74315
Jithin C <jithin3290@gmail.com>
2025-07-31 15:23:31 +0530

    Time: 1 ms (62.74%) | Memory: 55.5 MB (34.28%) - LeetSync

---
+++ 3396-valid-word/valid-word.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+    if (word.length < 3) return false;
+
+    let hasVowel = false;
+    let hasConsonant = false;
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+
+    for (let ch of word) {
+        if (!/[a-zA-Z0-9]/.test(ch)) return false;
+
+        if (/[a-zA-Z]/.test(ch)) {
+            if (vowels.has(ch)) {
+                hasVowel = true;
+            } else {
+                hasConsonant = true;
+            }
+        }
+    }
+
+    return hasVowel && hasConsonant;
+};
