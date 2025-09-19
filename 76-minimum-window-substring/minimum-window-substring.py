1fd72593706613e30221e02b7d829061ac306ec9
Jithin C <jithin3290@gmail.com>
2025-09-19 15:02:35 +0530

    Time: 122 ms (47.03%) | Memory: 12.7 MB (76.38%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,46 @@
+from collections import Counter
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if not s or not t:
+            return ""
+        
+        dict_t = Counter(t)  
+        required = len(dict_t)  
+
+        left, right = 0, 0
+        formed = 0  
+        window_counts = {}
+
+        min_len = float('inf')
+        min_window = (0, 0)
+
+        while right < len(s):
+            char = s[right]
+            window_counts[char] = window_counts.get(char, 0) + 1
+
+            if char in dict_t and window_counts[char] == dict_t[char]:
+                formed += 1
+
+            while left <= right and formed == required:
+                char = s[left]
+
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    min_window = (left, right)
+
+                window_counts[char] -= 1
+                if char in dict_t and window_counts[char] < dict_t[char]:
+                    formed -= 1
+
+                left += 1
+
+            right += 1
+
+        l, r = min_window
+        return s[l:r+1] if min_len != float('inf') else ""
